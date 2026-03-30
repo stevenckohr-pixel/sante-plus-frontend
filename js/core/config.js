0fbe038b816d1b5f4b649ed266b2f1612810ed50
stevenckohr-pixel <stevenckohr@gmail.com>
2026-03-30 09:45:11 +0100

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -3,8 +3,8 @@ export const CONFIG = {
   API_URL:
     window.location.hostname === "localhost" ||
     window.location.hostname === "127.0.0.1"
-      ? "https://stevenckohr-pixel.github.io/sante-plus-frontend//api"
-      : "https://sante-plus-frontend.onrender.com/api",
+      ? "https://stevenckohr-pixel.github.io/sante-plus-frontend/api"
+      : "https://sante-plus-backend-ux1n.onrender.com/api",
 
   APP_NAME: "Santé Plus Services",
   THEME_COLOR: "#16a34a", // Vert Santé
