c90e8fba11fda7275a415bbc28188be4a89d9f37
stevenckohr-pixel <stevenckohr@gmail.com>
2026-03-31 17:40:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -28,8 +28,8 @@ self.addEventListener("push", function (event) {
   const data = event.data.json();
   const options = {
     body: data.message,
-    icon: "https://cdn-icons-png.flaticon.com/512/9752/9752284.png",
-    badge: "https://cdn-icons-png.flaticon.com/512/9752/9752284.png",
+    icon: "https://res.cloudinary.com/dglwrrvh3/image/upload/v1774974945/heart-beat_tjb16u.png",
+    badge: "https://res.cloudinary.com/dglwrrvh3/image/upload/v1774974945/heart-beat_tjb16u.png",
     vibrate: [100, 50, 100],
     data: { url: data.url }
   };
