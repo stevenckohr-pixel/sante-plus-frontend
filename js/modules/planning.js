af1b74b1ada50998bf96b84a0c6f115d42818966
stevenckohr-pixel <stevenckohr@gmail.com>
2026-04-02 14:25:49 +0100

    Update planning.js

--- js/modules/planning.js
+++ js/modules/planning.js
@@ -419,15 +419,7 @@ window.submitAssignment = async () => {
 /**
  * 🔧 Échapper les caractères HTML
  */
-function escapeHtml(str) {
-    if (!str) return '';
-    return str
-        .replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#39;');
-}
+
 /**
  * 💡 TRANSITION INTELLIGENTE - Briefing
  */
