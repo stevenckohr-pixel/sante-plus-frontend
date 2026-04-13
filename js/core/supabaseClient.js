19d23c1fd5c88f89f57a8125b6d0d48b7c87ec24
stevenckohr-pixel <stevenckohr@gmail.com>
2026-04-13 11:59:34 +0200

    Update supabaseClient.js

--- js/core/supabaseClient.js
+++ js/core/supabaseClient.js
@@ -2,16 +2,14 @@
  * 🛰️ CONFIGURATION SUPABASE FRONTEND (Realtime)
  */
 
-// On récupère l'instance Supabase chargée via le CDN dans index.html
 const { createClient } = window.supabase;
 
-window._supabaseInstance = supabase;
-
-
 const supabaseUrl = "https://tagqwwfbpfzluahboczh.supabase.co";
-
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhZ3F3d2ZicGZ6bHVhaGJvY3poIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MDYxMDYsImV4cCI6MjA5MDM4MjEwNn0.I0HqBYPTrxPOg41sEWm_hU7YY3f9ZXCekUX5NlgIBWw"; 
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhZ3F3d2ZicGZ6bHVhaGJvY3poIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MDYxMDYsImV4cCI6MjA5MDM4MjEwNn0.I0HqBYPTrxPOg41sEWm_hU7YY3f9ZXCekUX5NlgIBWw";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+// ✅ Partager l'instance pour éviter les doublons GoTrueClient
+window._supabaseInstance = supabase;
 
 export default supabase;
