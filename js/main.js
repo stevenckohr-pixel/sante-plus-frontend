acb0c9aa2063819b70586274d82d09430ef8f09f
stevenckohr-pixel <stevenckohr@gmail.com>
2026-04-21 14:52:18 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1499,7 +1499,7 @@ if (authLogo) {
                 <div class="shrink-0 text-center pt-8 pb-4">
 <div class="flex justify-center mb-4">
     <div class="px-4 py-1 rounded-xl border-2 border-emerald-500 bg-white">
-        <img id="auth-logo-img" src="/sante-plus-frontend/assets/images/logo-general-icon.png" class="w-20 h-20 object-contain">
+        <img id="auth-logo-img" src="/sante-plus-frontend/assets/images/logo-general-icon.png" class="w-28 h-28 object-contain">
     </div>
 </div>
                     <p id="auth-step-title" class="text-slate-400 text-[8px] font-black uppercase tracking-[0.3em] mt-1.5">${stepTitle}</p>
